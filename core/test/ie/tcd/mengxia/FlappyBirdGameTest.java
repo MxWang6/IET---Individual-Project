02691d0f5c9129abfdd1f7a3f8c76466cc930a65
Mengxia Wang <wangm1@tcd.ie>
2017-01-13 10:00:50 +0000

    Add the unit test framework

---
+++ core/test/ie/tcd/mengxia/FlappyBirdGameTest.java
@@ -0,0 +1,12 @@
+package ie.tcd.mengxia;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FlappyBirdGameTest {
+
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
+}
